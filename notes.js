456c562889fac3ee82cbdffb4787a0854de3b9a5
Ali Paradise <alison.paradise@gmail.com>
2021-11-17 13:29:48 -0600

    build array filled with objects

---
+++ notes.js
@@ -0,0 +1,22 @@
+const notes = [
+{   id: 1,
+    subject: "Group Project",
+    date: 11.12,
+    feeling: "take a nap",
+    timeSpent: 5,
+},    
+{
+    id: 2,
+    subject: "GitHub",
+    date: 11.15,
+    feeling: "confused, then excited",
+    timeSpent: 2, 
+},
+{
+    id: 3,
+    subject: "JavaScript",
+    date: 11.16,
+    feeling: "take it slow",
+    timeSpent: 1,
+} 
+]
